883b766afab887f8fd329a9a0410e030c01f5835
Anh-Vu Nguyen <anh-vu.nguyen@telecom-paris.fr>
2020-01-04 12:53:53 +0100

    removed unused imports

--- 3DMAZE_Samuel_AnhVu/src/View/MiniMapBox.java
+++ 3DMAZE_Samuel_AnhVu/src/View/MiniMapBox.java
@@ -3,8 +3,6 @@ package View;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
