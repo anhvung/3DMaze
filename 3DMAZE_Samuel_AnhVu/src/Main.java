1513172586338fd2dcabd28bd65f77c01bc70178
Anh-Vu Nguyen <anh-vu.nguyen@telecom-paris.fr>
2020-01-05 13:37:00 +0100

    debug

--- 3DMAZE_Samuel_AnhVu/src/Main.java
+++ 3DMAZE_Samuel_AnhVu/src/Main.java
@@ -2,16 +2,24 @@ import org.jogamp.java3d.VirtualUniverse;
 
 public class Main {
 	public static String version;
+	public static boolean debug = true;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setErr(System.out);// java 9+ déclenchent des warnings à cause des .jar
-		new VirtualUniverse();
-		version= (String) VirtualUniverse.getProperties().get("j3d.version");
-		System.out.println("JAVA3D version " +version);
-		System.out.println("Tested on java 8 (1.8.0_221) and 11.0.5 on windows. You are currently running : "+System.getProperty("java.version"));
+		if (debug) {
+
+			// java 9+ déclenchent des warnings à cause des .jar
+			new VirtualUniverse();
+			version = (String) VirtualUniverse.getProperties().get("j3d.version");
+			System.out.println("JAVA3D version : " + version);
+			System.out.println("jogl version : " + com.jogamp.opengl.JoglVersion.getInstance());
+			System.out.println("Tested on java 8 (1.8.0_221) and 11.0.5 on windows. You are currently running : "
+					+ System.getProperty("java.version"));
+		}
+		else
+			System.err.close();
 		game.MainExecuteHere.main();
-		 
-		
+
 	}
 
 }
